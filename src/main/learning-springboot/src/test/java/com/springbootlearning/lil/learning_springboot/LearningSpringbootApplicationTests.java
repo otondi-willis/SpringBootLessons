1af6a324ee0385a4959873d340724975f29a91f4
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-08 10:41:00 +0300

    spring boot java test file

---
+++ src/main/learning-springboot/src/test/java/com/springbootlearning/lil/learning_springboot/LearningSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootlearning.lil.learning_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
